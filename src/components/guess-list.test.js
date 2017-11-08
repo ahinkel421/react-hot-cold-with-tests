b7cfdcb7eed70b88aa66ad26518e809a9d84036c
Adam Hinkel <ahinkel421@gmail.com>
2017-11-08 16:29:03 -0500

    test updates

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -7,4 +7,10 @@ describe('<GuessList />', () => {
 	it('Renders without crashing', () => {
 		shallow(<GuessList />);
 	});
+	it('Renders a list of the user\'s guesses', () => {
+		const values = [1, 2, 3];
+		const wrapper = shallow(<GuessList guesses={values} />);
+		const listItem = wrapper.find('li');
+		expect(listItem.length).toEqual(values.length);
+	});
 });
